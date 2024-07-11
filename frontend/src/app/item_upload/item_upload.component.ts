391be3e98d2493844b04355c52472453ea789b8b
Totengeist <me@totengeist.com>
2024-07-11 10:13:21 -0700

    frontend: Add item upload and revision page

---
+++ frontend/src/app/item_upload/item_upload.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { environment } from '../../environments/environment';
+import Uppy from '@uppy/core';
+import Form from '@uppy/form';
+import Dashboard from '@uppy/dashboard';
+import XHR from '@uppy/xhr-upload';
+
+@Component({
+  selector: 'app-item-upload',
+  templateUrl: './item_upload.component.html',
+  styleUrls: ['./item_upload.component.css']
+})
+export class ItemUploadComponent implements OnInit {
+  supportedTypes: any = {ship: ["ship file", [".ship"]], save: ["save file", [".space"]], modification: ["mod archive", [".zip"]]}; // eslint-disable-line @typescript-eslint/no-explicit-any
+  itemType = "";
+  parent = "";
+  uppy: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    let availableTypes: string[] = [];
+    for (const [_, value] of Object.entries(this.supportedTypes)) {
+      availableTypes = availableTypes.concat((value as any)[1]);
+    }
+    let selectedTypes = availableTypes;
+
+    if( this.route.snapshot.paramMap.get('parent') !== null ) {
+      this.parent = this.route.snapshot.paramMap.get('parent') ?? "";
+    }
+    if( this.route.snapshot.paramMap.get('itemType') !== null ) {
+      const typeCheck = this.route.snapshot.paramMap.get('itemType') ?? "";
+      console.log(typeCheck);
+      console.log(availableTypes);
+      if (typeCheck in this.supportedTypes) {
+        this.itemType = typeCheck;
+        selectedTypes = (this.supportedTypes as any)[typeCheck][1];
+      }
+    }
+    console.log(this.supportedTypes.ship);
+    console.log(selectedTypes);
+
+    this.uppy = new Uppy({
+      restrictions: {
+        allowedFileTypes: selectedTypes,
+        maxNumberOfFiles: 1,
+      },
+    })
+      .use(Dashboard, { inline: true, target: '#uppy' })
+      .use(Form, {
+        target: '#upload',
+      })
+      .use(XHR, { endpoint: environment.apiUrl+this.itemType })
+      .on('file-added', (file) => {
+        switch (file.extension) {
+        case 'space':
+          this.itemType = 'save';
+          break;
+        case 'ship':
+          this.itemType = 'ship';
+          break;
+        case 'zip':
+          this.itemType = 'modification';
+          break;
+        }
+        let endpoint = environment.apiUrl+this.itemType;
+        if( this.parent != "" ) {
+          endpoint += "/"+this.parent+"/upgrade";
+        }
+        console.log(endpoint);
+        this.uppy.getPlugin('XHRUpload').setOptions({ endpoint });
+      })
+      .on('file-removed', () => {
+        this.itemType = '';
+      })
+      .on('upload-success', (file, response) => {
+        this.router.navigate(['/'+this.itemType+'/'+response.body!.ref]);
+      });
+  }
+
+}
