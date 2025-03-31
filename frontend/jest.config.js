ae163d1dbae3452a557428651315b5b3ed8509be
Totengeist <me@totengeist.com>
2025-03-31 14:16:33 -0700

    Update SonarQube code coverage configuration

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-  coverageReporters: ['clover', 'text', 'html'],
+  coverageReporters: ['lcov', 'clover', 'text', 'html'],
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: ["item_upload", "item_edit"],
 }
